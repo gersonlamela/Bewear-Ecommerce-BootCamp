0c0fb61dd47840a81560052154fa8e12b44d1dd5
Gerson Lamela <gersonlamela7@gmail.com>
2025-08-05 01:12:49 +0100

    chore: add drizzle setup with initial schema; add seed script

---
+++ src/db/schema.ts
@@ -0,0 +1,67 @@
+import { relations } from "drizzle-orm";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+export const userTable = pgTable("user", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull(),
+});
+
+export const categoryTable = pgTable("category", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const categoryRelations = relations(categoryTable, ({ many }) => ({
+  products: many(productTable),
+}));
+
+export const productTable = pgTable("product", {
+  id: uuid().primaryKey().defaultRandom(),
+  categoryId: uuid("category_id")
+    .notNull()
+    .references(() => categoryTable.id),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  description: text().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const productsRelations = relations(productTable, ({ one, many }) => ({
+  category: one(categoryTable, {
+    fields: [productTable.categoryId],
+    references: [categoryTable.id],
+  }),
+  varients: many(productVarientTable),
+}));
+
+export const productVarientTable = pgTable("product_varient", {
+  id: uuid().primaryKey().defaultRandom(),
+  productId: uuid("product_id")
+    .notNull()
+    .references(() => productTable.id),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  imageUrl: text("image_url").notNull(),
+  color: text().notNull(),
+  priceInCents: integer("price_in_cents").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const productsVarientsRelations = relations(
+  productVarientTable,
+  ({ one }) => ({
+    product: one(productTable, {
+      fields: [productVarientTable.productId],
+      references: [productTable.id],
+    }),
+  }),
+);
