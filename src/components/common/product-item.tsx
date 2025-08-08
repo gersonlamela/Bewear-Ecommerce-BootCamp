a5beb99883c979b241ee2317a99a96f9a5774548
Gerson Lamela <gersonlamela7@gmail.com>
2025-08-08 15:50:34 +0100

    feat: add category page

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -1,16 +1,22 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCents } from "@/helpers/format-cents";
+import { cn } from "@/lib/utils";
 
 interface ProductListProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  textContainerClassname?: string;
 }
 
-export default function ProductItem({ product }: ProductListProps) {
+export default function ProductItem({
+  product,
+  textContainerClassname,
+}: ProductListProps) {
   const firstVariant = product.variants[0];
   console.log("Image URL:", firstVariant.imageUrl);
   return (
@@ -18,11 +24,17 @@ export default function ProductItem({ product }: ProductListProps) {
       <Image
         src={firstVariant.imageUrl}
         alt={firstVariant.name}
-        width={200}
-        height={260}
-        className="rounded-3xl"
+        sizes="100vw"
+        height={0}
+        width={0}
+        className="h-auto w-full rounded-3xl"
       />
-      <div className="flex max-w-[200px] flex-col gap-1">
+      <div
+        className={cn(
+          "flex max-w-[200px] flex-col gap-1",
+          textContainerClassname,
+        )}
+      >
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product.description}
